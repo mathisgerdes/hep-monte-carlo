5ea88f4d06631823e04d4175d14039c6981adc60
Timo Janssen <timo.janssen@theorie.physik.uni-goettingen.de>
2018-02-26 13:44:19 +0100

    bugfix

--- examples/importance_sampler.py
+++ examples/importance_sampler.py
@@ -3,9 +3,9 @@ from densities.camel import Camel
 from proposals.gaussian import Gaussian
 #from plotting.plot_1d import plot_1d
 from plotting.plot_2d import plot_2d
-from numpy.random import seed
+import numpy as np
 
-seed(1234)
+np.random.seed(1234)
 
 ndim = 2
 nsamples = 2000
@@ -17,7 +17,7 @@ is_proposal_weights = [0.5, 0.5]
 importance_sampler =  StaticMultiChannelImportanceSampler(ndim, target.pdf, is_proposal_dists, is_proposal_weights)
 
 start = np.full(ndim, 0.5)
-samples = sampler.sample(nsamples, ndim*[0.5])
+samples = importance_sampler.sample(nsamples, ndim*[0.5])
 
 #plot_1d(samples, target.pdf, mapping_pdf=importance_sampler.proposal_dist.pdf)
 plot_2d(samples, target.pdf)

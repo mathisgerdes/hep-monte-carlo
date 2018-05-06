c45537804841ec0e73ecf41de489839f26bc931f
Mathis Gerdes <mathisgerdes@gmail.com>
2018-05-06 19:32:08 +0200

    Update hmc example.

--- examples/test_hmc.py
+++ examples/test_hmc.py
@@ -1,10 +1,5 @@
-from samplers.mcmc.hmc import StaticHMC
-#from samplers.mcmc.hmc import DualAveragingHMC
-#from samplers.mcmc.nuts import NUTS
-from densities.camel import UnconstrainedCamel
-from proposals.gaussian import IsotropicZeroMeanGaussian
-#from plotting.plot_1d import plot_1d
-from plotting.plot_2d import plot_2d
+from monte_carlo.plotting.plot_2d import plot_2d
+from monte_carlo import *
 import numpy as np
 
 np.seterr(all='warn')
@@ -14,20 +9,23 @@ ndim = 2
 nsamples = 1000
 nadapt = 500
 
+
 def hmc_adapt_schedule(t):
     if t > nadapt:
         return False
     else:
         return True
 
-target = UnconstrainedCamel()
+
+target = densities.UnconstrainedCamel(ndim)
 #sampler = StaticHMC(ndim, target.pdf, target.log_pdf_gradient, 0.01, 0.1, 10, 150)
-sampler = StaticHMC(ndim, target.pdf, target.log_pdf_gradient, 0.05, 0.1, 10, 30, momentum=IsotropicZeroMeanGaussian(ndim, 1))
+sampler = hamiltonian.HamiltonianUpdate(
+    target, densities.Gaussian(ndim), 20, 0.05)
 start = np.full(ndim, 0.44)
 #sampler = DualAveragingHMC(ndim, target.pdf, target.log_pdf_gradient, 1, start, hmc_adapt_schedule, momentum=IsotropicZeroMeanGaussian(ndim, 1))
 #sampler = NUTS(ndim, target.pdf, target.log_pdf_gradient, start, hmc_adapt_schedule, momentum=IsotropicZeroMeanGaussian(ndim, 1))
 
-samples = sampler.sample(nsamples, start)
+samples = sampler.sample(nsamples, start).data
 #samples = [sample[0] for sample in samples]
 #print(samples)
 

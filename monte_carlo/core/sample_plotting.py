f16f75b9a62a3e6285293ed49f869becf6ede0f3
Mathis Gerdes <mathisgerdes@gmail.com>
2018-05-08 23:04:29 +0200

    Modify sample plotting.

--- monte_carlo/core/sample_plotting.py
+++ monte_carlo/core/sample_plotting.py
@@ -48,7 +48,7 @@ def plot2d(sample, target=None):
     fig = plt.figure(figsize=(14, 7))
 
     width = 2 if target is None else 3
-    ax1 = plt.subplot2grid((3, width), (0, 0), colspan=width)
+    ax1 = plt.subplot2grid((3, 3), (0, 0), colspan=3)
     ax1.set_title('time series')
     ax1.plot(sample.data[:, 0], label="x")
     ax1.plot(sample.data[:, 1], label="y")
@@ -58,31 +58,41 @@ def plot2d(sample, target=None):
     # guess a good binning
     bins = int(max(10, 20 * np.sqrt(sample.size / 1000)))
 
-    ax2 = plt.subplot2grid((3, width), (1, 0), rowspan=2)
+    ax2 = plt.subplot2grid((3, 3), (1, 0), rowspan=2)
     ax2.set_title('distribution')
     counts, xedges, yedges, im = ax2.hist2d(*sample.data.transpose(), bins=bins)
+    if target is not None:
+        extent = (xedges[0], xedges[-1], yedges[0], yedges[-1])
+        x = np.linspace(extent[0], extent[1], bins*10)
+        y = np.linspace(extent[2], extent[3], bins*10)
+        mgrid = np.meshgrid(x, y)
+        ax2.contour(x, y, target(*mgrid))
     plt.colorbar(im, ax=ax2)
     ax2.set_aspect('equal')
 
     # plot
-    ax3 = plt.subplot2grid((3, width), (1, width-1))
+    ax3 = plt.subplot2grid((3, 3), (1, 2))
     ax3.set_title('lag autocor. x')
     plot_lag_autocor(ax3, sample.data[:, 0])
 
-    ax4 = plt.subplot2grid((3, width), (2, width-1))
+    ax4 = plt.subplot2grid((3, 3), (2, 2))
     ax4.set_title('lag autocor. y')
     plot_lag_autocor(ax4, sample.data[:, 1])
 
-    if target is not None:
-        ax5 = plt.subplot2grid((3, width), (1, 1), rowspan=2)
-        ax5.set_title("target pdf")
-        extent = (xedges[0], xedges[-1], yedges[0], yedges[-1])
-        x = np.linspace(extent[0], extent[1], bins)
-        y = np.linspace(extent[2], extent[3], bins)
-        mgrid = np.meshgrid(x, y)
-        im = ax5.imshow(target(*mgrid), extent=extent, origin='lower')
-        plt.colorbar(im, ax=ax5)
-        ax5.set_aspect('equal')
+    ax5 = plt.subplot2grid((3, 3), (1, 1), rowspan=2)
+    ax5.set_title('scatter plot')
+    ax5.scatter(*sample.data.transpose())
+    ax5.grid()
+    # if target is not None:
+    #     ax5 = plt.subplot2grid((3, width), (1, 1), rowspan=2)
+    #     ax5.set_title("target pdf")
+    #     extent = (xedges[0], xedges[-1], yedges[0], yedges[-1])
+    #     x = np.linspace(extent[0], extent[1], bins)
+    #     y = np.linspace(extent[2], extent[3], bins)
+    #     mgrid = np.meshgrid(x, y)
+    #     im = ax5.imshow(target(*mgrid), extent=extent, origin='lower')
+    #     plt.colorbar(im, ax=ax5)
+    #     ax5.set_aspect('equal')
 
     fig.tight_layout()
     return fig

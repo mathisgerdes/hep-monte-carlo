f3d6b0f1a32b14865830bc00a0bfae1450368354
Timo Janssen <timo.janssen@theorie.physik.uni-goettingen.de>
2018-05-11 19:00:06 +0200

    fix ess for d>1

--- monte_carlo/core/util/effective_sample_size.py
+++ monte_carlo/core/util/effective_sample_size.py
@@ -1,26 +1,24 @@
 import numpy as np
+from .helper import interpret_array
 
-
-def effective_sample_size(samples, mean, var):
-    shape = samples.shape
-    nsamples = shape[0]
-    ndim = shape[1]
-    sum = np.zeros(ndim)
-    for dim in range(ndim):
+def effective_sample_size(sample, mean, var):
+    mean = interpret_array(mean, sample.ndim)
+    var = interpret_array(var, sample.ndim)
+    sum = np.zeros(sample.ndim)
+    for dim in range(sample.ndim):
         lag = 1
-        rho = autocorr(samples[:, dim], mean, var, lag)
-        while rho >= 0.05 and lag < nsamples-1:
-            sum[dim] = sum[dim] + (1-lag/nsamples)*rho
+        rho = autocorr(sample.data[:, dim], sample.size, mean[:, dim], var[:, dim], lag)
+        while rho >= 0.05 and lag < sample.size-1:
+            sum[dim] = sum[dim] + (1-lag/sample.size)*rho
             lag = lag + 1
-            rho = autocorr(samples[:, dim], mean, var, lag)
+            rho = autocorr(sample.data[:, dim], sample.size, mean[:, dim], var[:, dim], lag)
         
-    return nsamples / (1 + 2*sum)
+    return sample.size / (1 + 2*sum)
 
 
-def autocorr(samples, mean, var, lag=1):
-    nsamples = len(samples)
+def autocorr(sample, size, mean, var, lag=1):
     sum = 0
-    for m in range(lag, nsamples):
-        sum = sum + (samples[m] - mean) * (samples[m-lag] - mean)
+    for m in range(lag, size):
+        sum = sum + (sample[m] - mean) * (sample[m-lag] - mean)
         
-    return sum / (var*(nsamples-lag))
+    return sum / (var*(size-lag))

e96cb3c0954650996f33598d722876a87c14df25
Mathis Gerdes <mathisgerdes@gmail.com>
2018-05-11 14:34:59 +0200

    Fix module imports for banana.

--- monte_carlo/core/densities/__init__.py
+++ monte_carlo/core/densities/__init__.py
@@ -1,5 +1,6 @@
 from .gaussian import Gaussian
 from .camel import Camel, UnconstrainedCamel
 from .uniform import Uniform
+from .banana import Banana
 
-__all__ = ['Gaussian', 'Camel', 'UnconstrainedCamel', 'Uniform']
+__all__ = ['Gaussian', 'Camel', 'UnconstrainedCamel', 'Uniform', 'Banana']

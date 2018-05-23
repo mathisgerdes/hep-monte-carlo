cf383dda6aeaf6b33f35993e4994e0a79db43a83
Mathis Gerdes <mathisgerdes@gmail.com>
2018-05-23 19:10:13 +0200

    Make sampling multi-core (if available).

--- make_sample.py
+++ make_sample.py
@@ -1,122 +1,129 @@
 import sys
 import os
 import json
+from multiprocessing import Pool
 import numpy as np
 from monte_carlo import *
 
+sampler_module = None
+save_base = None
 
-def run(config, dir_base):
-    save_base = os.path.join(dir_base, config['name'])
-    # make sure save directory exists
-    if not os.path.exists(save_base):
-        os.makedirs(save_base)
 
-    # load sampler module
-    sampler_module = getattr(interfaces, config['sampler'])
+def run(config):
+    # parse params
+    for key in config['params']:
+        config['params'][key] = eval(config['params'][key])
     if 'params_vary' in config:
-        run_all(config, save_base, sampler_module)
+        for key in config['params_vary']:
+            config['params_vary'][key] = eval(config['params_vary'][key])
+
+    if 'params_vary' in config:
+        runner = RunIterator(config)
+        runner.run()
     else:
-        run_single(config, save_base, sampler_module)
+        print('START SINGLE ' + config['name'], flush=True)
+        run_single(config)
 
 
-def run_single(config, save_base, sampler_module):
-    print(config['name'], flush=True)
-    params = config['params']
-    for param in params:
-        params[param] = eval(params[param])
+def run_single(config, params=None, name='sample'):
+    if params is None:
+        params = config['params']
+    print("STARTING RUN " + str(params))
 
-    if 'binning' in config:
-        binning = eval(config['binning'])
-    else:
-        binning = None
-
-    target = eval(config['target'])(params['ndim'],
-                                    **eval(config['target_args']))
+    target_class = eval(config['target'])
+    target = target_class(params['ndim'], **eval(config['target_args']))
     util.count_calls(target, 'pdf', 'pot_gradient')
 
     sampler, init = sampler_module.get_sampler(target, **params)
-    sample = sampler.sample(config['size'], init)
-
-    if binning is not None:
-        sample._bin_wise_chi2 = util.bin_wise_chi2(sample, *binning)
-
-    print(sample, flush=True)
-    sample.save(os.path.join(save_base, 'sample'))
-
-
-def run_all(config, save_base, sampler_module):
-    print(config['name'], flush=True)
-    params = config['params']
-    for param in params:
-        params[param] = eval(params[param])
-
-    vary_names = config['params_vary'].keys()
-    vary_values = [eval(v) for v in config['params_vary'].values()]
+    sample = sampler.sample(config['size'], init, log_every=-1)
 
     if 'binning' in config:
         binning = eval(config['binning'])
-    else:
-        binning = None
+        sample._bin_wise_chi2 = util.bin_wise_chi2(sample, *binning)
 
-    results = {
-        'params_vary': dict(zip(vary_names,
-                                [np.array(v).tolist() for v in vary_values])),
-        'params': params,
-        'sample_size': config['size'],
-        'mean': [],
-        'variance': [],
-        'chi2': [],
-        'chi2_p': [],
-        'chi2_n': [],
-        'eff_sample_size': [],
-        'pdf_calls': [],
-        'pot_gradient_calls': [],
-        'accept_rate': []
-    }
+    print("FINISHED " + str(params) + ':\n' + str(sample), flush=True)
+    sample.save(os.path.join(save_base, name))
 
-    index = 0
-    for param_it in zip(*vary_values):
-        kwargs = dict(zip(vary_names, param_it))
-        kwargs.update(params)
+    info = {'mean': np.array(sample.mean).tolist(),
+            'variance': np.array(sample.variance).tolist(),
+            'pdf_calls': target.pdf.count,
+            'pdf_gradient_calls': target.pdf.count,
+            'eff_sample_size': np.array(sample.effective_sample_size).tolist(),
+            'accept_rate': sample.accept_ratio,
+            'chi2': sample.bin_wise_chi2[0],
+            'chi2_p': sample.bin_wise_chi2[1],
+            'chi2_n': sample.bin_wise_chi2[2]}
+    return info
 
-        target = eval(config['target'])(kwargs['ndim'], **eval(config['target_args']))
-        util.count_calls(target, 'pdf', 'pot_gradient')
 
-        sampler, init = sampler_module.get_sampler(target, **kwargs)
-        print(config['name'] + ", run %d" % (index + 1), flush=True)
-        sample = sampler.sample(config['size'], init)
+class RunIterator(object):
+    def __init__(self, config):
+        self.save_base = save_base
+        self.config = config
 
-        # should be evaluated before calling bin chi^2
-        results['pdf_calls'].append(target.pdf.count)
-        results['pot_gradient_calls'].append(target.pot_gradient.count)
+        self.params = config['params']
 
-        results['mean'].append(np.array(sample.mean).tolist())
-        results['variance'].append(np.array(sample.variance).tolist())
-        if binning is None:
-            chi2, chi2_p, chi2_n = sample.bin_wise_chi2
-        else:
-            chi2, chi2_p, chi2_n = util.bin_wise_chi2(sample, *binning)
-            sample._bin_wise_chi2 = (chi2, chi2_p, chi2_n)
+        self.vary_names = list(config['params_vary'].keys())
+        self.vary_values = list(config['params_vary'].values())
 
-        results['chi2'].append(chi2)
-        results['chi2_p'].append(chi2_p)
-        results['chi2_n'].append(chi2_n)
-        results['eff_sample_size'].append(
-            np.array(sample.effective_sample_size).tolist())
-        results['accept_rate'].append(sample.accept_ratio)
+    def run_iter(self, index):
+        vary_values = [val[index] for val in self.vary_values]
+        params_iter = dict(zip(self.vary_names, vary_values))
+        params_iter.update(self.params)
+        return run_single(self.config, params_iter, 'run-%d' % index)
 
-        sample.save(os.path.join(save_base, 'run-%d' % index))
+    def run(self):
+        print('START ' + self.config['name'], flush=True)
+
+        var_val_listed = [np.array(v).tolist() for v in self.vary_values]
+        results = {'params_vary': dict(zip(self.vary_names, var_val_listed)),
+                   'params': self.params,
+                   'sample_size': self.config['size'],
+                   'seed': self.config['seed']}
+
+        indices = list(range(len(self.vary_values[0])))
+        with Pool() as pool:
+            infos = pool.map(self.run_iter, indices)
+
+        for info in infos:
+            for key in info:
+                try:
+                    results[key].append(info[key])
+                except KeyError:
+                    results[key] = [info[key]]
+
+        with open(self.save_base + '.json', 'w') as out_file:
+            json.dump(results, out_file, indent=2)
+
+def find_out_dir(config_file):
+    base = os.path.split(config_file)[0]
+    if not os.path.isdir(os.path.join(base, 'out')):
+        return os.path.join(base, 'out')
+    index = 1
+    while os.path.isdir(os.path.join(base, 'out-%d' % index)):
         index += 1
-
-    with open(save_base + '.json', 'w') as out_file:
-        json.dump(results, out_file, indent=2)
-
+    return os.path.join(base, 'out-%d' % index)
 
 if __name__ == '__main__':
-    np.random.seed(42)
     config_file = sys.argv[1]
-    base = os.path.split(config_file)[0]
+    dir_base = find_out_dir(config_file)
+    os.makedirs(dir_base)
+
     with open(config_file) as in_file:
         configs = json.load(in_file)
     for run_config in configs:
-        run(run_config, base)
+        # load sampler module
+        sampler_module = getattr(interfaces, run_config['sampler'])
+
+        # set, and make sure save directory exists
+        save_base = os.path.join(dir_base, run_config['name'])
+        if not os.path.exists(save_base):
+            os.makedirs(save_base)
+
+        # set random seed if given
+        if 'seed' not in run_config:
+            seed = np.random.randint(0, 2**32-1)
+            run_config['seed'] = seed
+        np.random.seed(run_config['seed'])
+
+        run(run_config)

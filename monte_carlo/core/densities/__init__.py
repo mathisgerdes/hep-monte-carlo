b003fdc52ef38e638ae64d0a545099f680d1bfa0
Mathis Gerdes <mathisgerdes@gmail.com>
2018-06-05 15:18:11 +0200

    Allow module import without Sherpa installation.

--- monte_carlo/core/densities/__init__.py
+++ monte_carlo/core/densities/__init__.py
@@ -3,7 +3,12 @@ from .camel import Camel, UnconstrainedCamel
 from .uniform import Uniform
 from .banana import Banana
 from .rambo import Rambo, RamboOnDiet
-from .qcd import ee_qq
+
+try:
+    from .qcd import ee_qq
+except ModuleNotFoundError:
+    print("Can't find Sherpa installation, skipping ee_qq.")
+    ee_qq = None
 
 __all__ = ['Gaussian', 'Camel', 'UnconstrainedCamel', 'Uniform', 'Banana',
            'Rambo', 'RamboOnDiet', 'ee_qq']
